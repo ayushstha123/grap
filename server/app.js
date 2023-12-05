4604f580f167d9adcc611f08e250ee991ecc3a96
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2023-12-05 21:43:13 +0545

    connected to mongodb

--- server/app.js
+++ server/app.js
@@ -1,15 +1,32 @@
-import  express from 'express';
+import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import schema from './schema/schema.js';
+import mongoose from 'mongoose';
 
-const app=express();
+const app = express();
 
-//making an api for express to understand graphql
-app.use('/graphql',graphqlHTTP({
+// Making an API for Express to understand GraphQL
+app.use(
+  '/graphql',
+  graphqlHTTP({
     schema,
-graphiql:true
-}))
+    graphiql: true,
+  })
+);
 
-app.listen(4000,()=>{
-    console.log("server is running on port 4000");
-})
+// Wrap the database connection code in an async function
+const connectToDatabase = async () => {
+  try {
+    const conn = await mongoose.connect('mongodb://graphql:graphql@ac-gicpmv3-shard-00-00.kdmeax6.mongodb.net:27017,ac-gicpmv3-shard-00-01.kdmeax6.mongodb.net:27017,ac-gicpmv3-shard-00-02.kdmeax6.mongodb.net:27017/?replicaSet=atlas-tdyglg-shard-0&ssl=true&authSource=admin');
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+  }
+};
+
+// Call the async function to connect to the database
+connectToDatabase();
+
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
